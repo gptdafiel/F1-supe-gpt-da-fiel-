06fc0f28b7a64aded05331e2740484da99b05754
gptdafiel <arthurspereira27@gmail.com>
2025-08-03 16:55:28 -0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("jogo");
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#111";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+ctx.fillStyle = "#fff";
+ctx.font = "32px Arial";
+ctx.fillText("F1 Super GPT da Fiel - EM BREVE!", 100, 300);
